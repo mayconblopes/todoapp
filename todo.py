1998befe1f37b3bab2c7e1b9fe21d46153042e36
mayconblopes <lopes.maycon@gmail.com>
2022-09-29 15:28:36 -0300

    Implemented EDIT and DELETE

--- todo.py
+++ todo.py
@@ -1,5 +1,70 @@
 import flet
-from flet import Checkbox, FloatingActionButton, Page, TextField, icons, Column, Row, UserControl
+from flet import Checkbox, FloatingActionButton, Page, TextField, icons, Column, Row, UserControl, IconButton, colors
+from requests import delete
+
+class Task(UserControl):
+    def __init__(self, task_name, task_delete):
+        super().__init__()
+        self.task_name = task_name
+        self.task_delete = task_delete
+    
+    def build(self):
+        self.display_task = Checkbox(value=False, label=self.task_name)
+        self.edit_name = TextField(expand=1)
+
+        self.display_view = Row(
+            alignment='spaceBetween',
+            vertical_alignment='center',
+            controls=[
+                self.display_task,
+                Row(
+                    spacing=0,
+                    controls=[
+                        IconButton(
+                            icon=icons.CREATE_OUTLINED,
+                            tooltip='Editar To-Do',
+                            on_click=self.edit_clicked,
+                        ),
+                        IconButton(
+                            icons.DELETE_OUTLINE,
+                            tooltip='Deletar To-Do',
+                            on_click=self.delete_clicked
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = Row(
+            visible=False,
+            alignment='spaceBetween',
+            vertical_alignment='center',
+            controls=[
+                self.edit_name,
+                IconButton(
+                    icon=icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=colors.GREEN,
+                    tooltip='Atualizar To-Do',
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return Column(controls=[self.display_view, self.edit_view])
+    
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+    
+    def delete_clicked(self, e):
+        self.task_delete(self)
 
 class TodoApp(UserControl):
     def build(self):
@@ -9,7 +74,6 @@ class TodoApp(UserControl):
         # app's root control
         root = Column(
             width=375,
-            height=667,
             controls=[
                 Row(
                     controls=[
@@ -24,9 +88,15 @@ class TodoApp(UserControl):
         return root
 
     def add_clicked(self, e):
-        self.tasks.controls.append(Checkbox(label=self.new_task.value))
+        task = Task(self.new_task.value, self.task_delete)
+        self.tasks.controls.append(task)
         self.new_task.value = '' 
         self.update()
+
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
 
 def main(page: Page):
     page.title = 'ToDo App'
