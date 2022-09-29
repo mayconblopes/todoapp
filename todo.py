b4e518b46468255c46acfaeafc2dd933ed0225a1
mayconblopes <lopes.maycon@gmail.com>
2022-09-28 22:44:49 -0300

    First version of a simple todo app

---
+++ todo.py
@@ -0,0 +1,15 @@
+import flet
+from flet import Checkbox, FloatingActionButton, Page, TextField, icons
+
+def main(page: Page):
+
+    def add_clicked(e):
+        page.add(Checkbox(label=new_task.value))
+        new_task.value=''
+        page.update()
+
+    new_task = TextField(hint_text='O que deve ser feito?')
+
+    page.add(new_task, FloatingActionButton(icon=icons.ADD, on_click=add_clicked))
+
+flet.app(target=main, port=8000, route_url_strategy='path')
