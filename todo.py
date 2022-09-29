93e880e9eb386c552272671a24ac60e80d015f60
mayconblopes <lopes.maycon@gmail.com>
2022-09-28 22:54:07 -0300

    Added Column and Row. Also aligned elements

--- todo.py
+++ todo.py
@@ -1,15 +1,29 @@
 import flet
-from flet import Checkbox, FloatingActionButton, Page, TextField, icons
+from flet import Checkbox, FloatingActionButton, Page, TextField, icons, Column, Row
 
 def main(page: Page):
 
     def add_clicked(e):
-        page.add(Checkbox(label=new_task.value))
+        tasks_view.controls.append(Checkbox(label=new_task.value))
         new_task.value=''
         page.update()
 
-    new_task = TextField(hint_text='O que deve ser feito?')
+    new_task = TextField(hint_text='O que deve ser feito?', expand=True)
+    tasks_view = Column()
+    view = Column(
+        width=600,
+        controls=[
+            Row(
+                controls=[
+                    new_task,
+                    FloatingActionButton(icon=icons.ADD, on_click=add_clicked),
+                ],
+            ),
+            tasks_view,
+        ],
+    )
 
-    page.add(new_task, FloatingActionButton(icon=icons.ADD, on_click=add_clicked))
+    page.horizontal_alignment = 'center'
+    page.add(view)
 
 flet.app(target=main, port=8000, route_url_strategy='path')
