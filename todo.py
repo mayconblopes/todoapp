cb7cc392bed2aecf078662620427e022da0fccec
mayconblopes <lopes.maycon@gmail.com>
2022-09-28 23:14:25 -0300

    Encapsulated code by creating a TodoApp class

--- todo.py
+++ todo.py
@@ -1,29 +1,40 @@
 import flet
-from flet import Checkbox, FloatingActionButton, Page, TextField, icons, Column, Row
+from flet import Checkbox, FloatingActionButton, Page, TextField, icons, Column, Row, UserControl
+
+class TodoApp(UserControl):
+    def build(self):
+        self.new_task = TextField(hint_text='O que deve ser feito?', expand=True)
+        self.tasks = Column()
+
+        # app's root control
+        root = Column(
+            width=375,
+            height=667,
+            controls=[
+                Row(
+                    controls=[
+                        self.new_task,
+                        FloatingActionButton(icon=icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        )
+
+        return root
+
+    def add_clicked(self, e):
+        self.tasks.controls.append(Checkbox(label=self.new_task.value))
+        self.new_task.value = '' 
+        self.update()
 
 def main(page: Page):
-
-    def add_clicked(e):
-        tasks_view.controls.append(Checkbox(label=new_task.value))
-        new_task.value=''
-        page.update()
-
-    new_task = TextField(hint_text='O que deve ser feito?', expand=True)
-    tasks_view = Column()
-    view = Column(
-        width=600,
-        controls=[
-            Row(
-                controls=[
-                    new_task,
-                    FloatingActionButton(icon=icons.ADD, on_click=add_clicked),
-                ],
-            ),
-            tasks_view,
-        ],
-    )
-
+    page.title = 'ToDo App'
     page.horizontal_alignment = 'center'
-    page.add(view)
+    page.update()
+
+    todo = TodoApp()
+
+    page.add(todo)
 
 flet.app(target=main, port=8000, route_url_strategy='path')
